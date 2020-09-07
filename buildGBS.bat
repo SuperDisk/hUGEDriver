efdf54921b2d61348ac4d3a98020395fe884d4eb
Nick <yux50000@hotmail.com>
2020-09-07 15:16:00 -0400

    Remove unused stuff

--- buildGBS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rgbasm -opreview.obj driverLite.z80
-if %errorlevel% neq 0 call :exit 1
-rgblink -opreview.gbs preview.obj
-if %errorlevel% neq 0 call :exit 1
-call :exit 0
-
-:exit
-exit
