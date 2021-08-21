1877d50329aef298df47f99710ab7a0ceedb1f3d
ISSOtm <eldredhabert0@gmail.com>
2021-08-21 17:51:27 +0200

    Output sym file for example as well

--- rgbds_example/build.bat
+++ rgbds_example/build.bat
@@ -9,5 +9,5 @@ rgbasm -osample_song.obj -i.. sample_song.asm
 rgbasm -oplayer.obj -i.. -DSONG_DESCRIPTOR=ryukenden ../player.asm
 
 :: Link the objects together and run rgbfix
-rgblink -ooutput.gb player.obj hUGEDriver.obj sample_song.obj
+rgblink -ooutput.gb -noutput.sym player.obj hUGEDriver.obj sample_song.obj
 rgbfix -p0 -fhg output.gb
