5eaed0eefbb64d465abe247a4d1b68e35afd42ea
Nick Faro <yux50000@hotmail.com>
2022-02-07 18:18:09 -0500

    Add uncap to hugedriver asm file

--- hUGEDriver.asm
+++ hUGEDriver.asm
@@ -1685,6 +1685,38 @@ IF DEF(PREVIEW_MODE)
 ENDC
     ret
 
+
+uncap::
+	ld	a,[hl+]			; start with token
+	and	a			; 0 marks the end of packed data
+	ret	z
+	bit	7,a			; next byte is offset if 7th bit is set
+	jr	z,_literals             ; literals come next otherwise
+	and	$7F			; bits 0 - 6 define reference length
+	ld	c,a			; move length to c(ounter)
+	ld	a,[hl+]			; load offset
+	push	hl			; store packed data address
+	ld	l,a			; offset is negative because game boy cpu lacks
+	ld	h,$FF			; sub hl,de :)
+	add	hl,de			; locate reference
+_1:
+	ld	a,[hl+]			; and copy it
+	ld	[de],a
+	inc	de
+	dec	c
+	jr	nz,_1
+	pop	hl			; restore packed data address
+	jr	uncap			; continue
+_literals:
+	ld	c,a			; move length to c(ounter)
+_2:
+	ld	a,[hl+]			; and copy c literals
+	ld	[de],a
+	inc	de
+	dec	c
+	jr	nz,_2
+	jr	uncap			; continue
+
 note_table:
 include "include/hUGE_note_table.inc"
 
