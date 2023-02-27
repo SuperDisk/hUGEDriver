2d8a642a79f5893910d1527a364064f5002d6033
Toxa <untoxa@mail.ru>
2023-02-27 23:28:15 +0300

    fix fx_note_cut

--- hUGEDriver.asm
+++ hUGEDriver.asm
@@ -894,6 +894,7 @@ fx_note_cut:
 
     ;; check channel mute
 
+    ld a, b
     ;; 0 → $01, 1 → $02, 2 → $04, 3 → $05
     ;; Overall, these two instructions add 1 to the number.
     ;; However, the first instruction will generate a carry for inputs of $02 and $03;
@@ -912,8 +913,9 @@ fx_note_cut:
     rra
     ld d, a
     ld a, [mute_channels]
+    cpl 
     and d
-    ret nz
+    ret z
 
     ;; fallthrough
 
