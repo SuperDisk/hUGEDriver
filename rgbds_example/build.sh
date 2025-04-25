a6d3433e0c990cd4173ddbaf56716a908cf24c92
ISSOtm <me@eldred.fr>
2025-04-25 13:33:09 -0400

    Use the new `-I` flag

--- rgbds_example/build.sh
+++ rgbds_example/build.sh
@@ -1,11 +1,11 @@
 # Assemble the hUGEDriver source into an object
-rgbasm -ohUGEDriver.obj -i.. ../hUGEDriver.asm
+rgbasm -ohUGEDriver.obj -I.. ../hUGEDriver.asm
 
 # Assemble the song into an object
-rgbasm -osample_song.obj -i../include sample_song.asm
+rgbasm -osample_song.obj -I../include sample_song.asm
 
 # Assemble the example player code into an object
-rgbasm -H -oplayer.obj -i.. -DSONG_DESCRIPTOR=sample_song ../player.asm
+rgbasm -oplayer.obj -I.. -DSONG_DESCRIPTOR=sample_song ../player.asm
 
 # Link the objects together and run rgbfix
 rgblink -ooutput.gb -noutput.sym player.obj hUGEDriver.obj sample_song.obj
